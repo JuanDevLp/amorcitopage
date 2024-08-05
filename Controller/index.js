a87cd378348d639093cacccc672431fb43caa18f
JuanDevLp <juandavidlopezrobles2@gmail.com>
2024-08-04 22:38:22 -0500

    AAA

---
+++ Controller/index.js
@@ -0,0 +1,15 @@
+actLove.addEventListener('click', (e)=>{
+    Swal.fire({
+        title: "TE AMO MUCHO MI AMOR",
+        width: 600,
+        padding: "3em",
+        color: "#716add",
+        background: "#fff url(/images/trees.png)",
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("/images/nyan-cat.gif")
+          left top
+          no-repeat
+        `
+      });
+})
